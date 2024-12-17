55890e922549b7dba032a122fa31676203011d52
meynardtrinidad <meynard.trinidad44@gmail.com>
2024-12-17 15:51:57 +0800

    feat: sort characters based on occurence

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import src.constants as c
 
 
+# FIXME: Get file statistics by going through the whole file once.
 class File:
     def __init__(self, file: io.TextIOWrapper):
         self.f = file
@@ -10,6 +11,7 @@ class File:
     def get_word_count(self) -> int:
         word_ctr = 0
 
+        self.f.seek(0)
         for l in self.f:
             for w in l.split():
                 if w.isalpha():
@@ -17,9 +19,10 @@ class File:
 
         return word_ctr
 
-    def get_letter_count(self) -> dict[str, int]:
-        record = {}
+    def get_char_count(self) -> list[dict[str, str | int]]:
+        char_count = {}
 
+        self.f.seek(0)
         for l in self.f:
             ln = l.lower()
             ln.isspace
@@ -28,11 +31,16 @@ class File:
                 if not ch.isalpha():
                     continue
 
-                if ch in record:
-                    record[ch] += 1
+                if ch in char_count:
+                    char_count[ch] += 1
                 else:
-                    record[ch] = 1
+                    char_count[ch] = 1
 
+        record = []
+        for k, v in char_count.items():
+            record.append({"char": k, "occurence": v})
+
+        record.sort(reverse=True, key=lambda x: x["occurence"])
         return record
 
 
@@ -41,11 +49,19 @@ def main():
         print("Usage: Include a file." + "python3 main.py <file-name>")
         sys.exit(c.ARGS_FILE_NOT_SUPPLIED)
 
-    f = open("./frankenstein.txt")
+    fn = sys.argv[1]
+    f = open(fn)
     file = File(f)
-    # print(file.get_word_count())
-    print(file.get_letter_count())
+    char_count = file.get_char_count()
+    word_count = file.get_word_count()
     f.close()
+
+    print(f"--- Begin report of {fn} ---")
+    print(f"{word_count} words found in the document.\n")
+
+    for ch in char_count:
+        print(f"The '{ch.get("char")}' character was found {ch.get("occurence")} times")
+    print(f"--- End report ---")
 
 
 if __name__ == "__main__":
