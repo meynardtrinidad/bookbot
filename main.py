07bed7a2667c62dc37ea94121845f859051b7670
meynardtrinidad <meynard.trinidad44@gmail.com>
2024-12-17 15:27:54 +0800

    feat: character counter

--- main.py
+++ main.py
@@ -3,19 +3,43 @@ import sys
 import src.constants as c
 
 
+def is_white_space(ch: str) -> bool:
+    if ch == "\n" or ch == "" or ch == " " or ch == "\t":
+        return True
+
+    return False
+
+
 class File:
     def __init__(self, file: io.TextIOWrapper):
         self.f = file
 
-    def get_length(self) -> int:
+    def get_word_count(self) -> int:
         word_ctr = 0
 
         for l in self.f:
             for w in l.split():
-                if w != "\n" and w != "":
+                if not is_white_space(w):
                     word_ctr += 1
 
         return word_ctr
+
+    def get_letter_count(self) -> dict[str, int]:
+        record = {}
+
+        for l in self.f:
+            ln = l.lower()
+            for i in range(len(ln)):
+                ch = ln[i]
+                if is_white_space(ch):
+                    continue
+
+                if ch in record:
+                    record[ch] += 1
+                else:
+                    record[ch] = 1
+
+        return record
 
 
 def main():
@@ -25,7 +49,8 @@ def main():
 
     f = open("./frankenstein.txt")
     file = File(f)
-    print(file.get_length())
+    # print(file.get_word_count())
+    print(file.get_letter_count())
     f.close()
 
 
